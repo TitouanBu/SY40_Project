a49a1dca682d3b67da59859be5ca25c9b73c6e7f
Titouan Burgy <titouan.burgy@laposte.net>
2022-05-27 16:32:38 +0200

    Ajout quelques fonctions pour Thread, fichier Consignes.txt avec consignes du sujet simplifié

--- headers/CodingFunctions.h
+++ headers/CodingFunctions.h
@@ -6,15 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <pthread.h>
 /*--------------------------------------------------*/
-
-#define false  0
-#define true  1
 
 /*------Mise en place des variables globales------*/
+#define false  0
+#define true  1
 bool activeDebug=false;	//Variable pour afficher ou enlever les debugs
+
+#define NUM_P_ABONNE 10	//Nombre de places réservées aux abonnés
+#define NUM_P_NABONNE 10	//Nombre de places pour non abonnés
 /*--------------------------------------------------*/
 
+//Fonction pour debug du code
 void debug (char *message)
 {
 	if (activeDebug)
@@ -22,5 +26,49 @@ void debug (char *message)
 		printf("\n%s\n",message);
 	}
 }
+
+
+/*------Fonctions pour gestion des threads ------*/
+//Fonction executee par chaque abonne
+void *fonc_abonne(void *n)
+{
+	printf("Abonne numero %d, TID : %ld\n",
+			(int) n,pthread_self());
+}
+
+//Fonction pour créer N abonne(s)
+void create_abonne(int N)
+{
+	pthread_t idAbonne[N];
+	for (int i = 0; i < N; ++i)
+	{
+		pthread_create(idAbonne+i,0,fonc_abonne, (void *) i);
+	}
+
+	for(int i=0;i<N;++i)
+        	pthread_join(idAbonne[i],NULL);
+}
+
+//Fonction executee par chaque non-abonne
+void *fonc_non_abonne(void *n)
+{
+	printf("Non-Abonne numero %d, TID : %ld\n",
+			(int) n,pthread_self());
+}
+
+//Fonction pour créer N abonne(s)
+void create_non_abonne(int N)
+{
+	pthread_t idNonAbonne[N];
+	for (int i = 0; i < N; ++i)
+	{
+		pthread_create(idNonAbonne+i,0,fonc_non_abonne, (void *) i);
+	}
+
+	for(int i=0;i<N;++i)
+        	pthread_join(idNonAbonne[i],NULL);
+}
+
+/*--------------------------------------------------*/
 
 #endif
--- main.c
+++ main.c
@@ -2,8 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
-	debug("Test 1");
-	isactive = false;
-	debug("test 2");
+	int nombre_abonne,nombre_n_abonne;
+	if(argc < 2){
+		nombre_abonne = 10;
+		nombre_n_abonne = 10;
+	}else if(argc == 2){
+		nombre_abonne = atoi(argv[1]);
+		nombre_n_abonne = atoi(argv[1]);
+	}else if (argc>2){
+		nombre_abonne = atoi(argv[1]);
+		nombre_n_abonne = atoi(argv[2]);
+	}
+
+	create_abonne(nombre_abonne);
+	create_non_abonne(nombre_n_abonne);
 	return 0;
 }
